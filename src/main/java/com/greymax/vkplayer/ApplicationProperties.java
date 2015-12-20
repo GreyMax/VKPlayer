f05bf009e78192142529c3d38452ea470e4e86e7
Alexander Pasechnik <davidofik88@gmail.com>
2015-12-20 23:06:20 +0200

    introduce application properties

---
+++ src/main/java/com/greymax/vkplayer/ApplicationProperties.java
@@ -0,0 +1,42 @@
+package com.greymax.vkplayer;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class ApplicationProperties {
+
+  private static final String APPLICATION_PROPERTIES_FILE = "application.properties";
+  private static ApplicationProperties instance = null;
+
+  private Properties properties = new Properties();
+
+  public static ApplicationProperties getInstance() {
+    if (null == instance) {
+      instance = new ApplicationProperties();
+    }
+    return instance;
+  }
+
+  private ApplicationProperties() {
+    try {
+      properties.load(this.getClass().getClassLoader().getResourceAsStream(APPLICATION_PROPERTIES_FILE));
+    } catch (IOException e) {
+      throw new RuntimeException("Can't read application properties", e);
+    }
+  }
+
+  public String getSting(Object key) {
+    Object property = properties.get(key);
+    return null == property ? null : property.toString();
+  }
+
+  public Integer getInt(Object key) {
+    Object property = properties.get(key);
+    return null == property ? null : Integer.valueOf(property.toString());
+  }
+
+  public Boolean getBoolean(Object key) {
+    Object property = properties.get(key);
+    return null == property ? null : Boolean.valueOf(property.toString());
+  }
+}
--- src/main/java/com/greymax/vkplayer/VKPlayer.java
+++ src/main/java/com/greymax/vkplayer/VKPlayer.java
@@ -5,19 +5,20 @@ import javafx.application.Application;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class VKPlayer extends Application{
+public class VKPlayer extends Application {
 
-  public static final String APPLICATION_TITLE = "VK Player";
-  public static final String APPLICATION_ICON = "image/ico.png";
+  public static final String VK_PLAYER_ICON = "image/ico.png";
+  public static final String VK_PLAYER_TITLE_KEY = "title";
 
   @Override
   public void start(Stage stage) throws Exception {
+    ApplicationProperties applicationProperties = ApplicationProperties.getInstance();
 //    Parent root = FXMLLoader.load(getClass().getResource("ui/main/index.fxml"));
 //    root.getStylesheets().add("/css/main/playlist.css");
 //    root.getStylesheets().add("/css/components/SearchBox.css");
-    stage.setTitle(APPLICATION_TITLE);
+    stage.setTitle(applicationProperties.getSting(VK_PLAYER_TITLE_KEY));
     stage.setResizable(Boolean.TRUE);
-    stage.getIcons().add(new Image(APPLICATION_ICON));
+    stage.getIcons().add(new Image(VK_PLAYER_ICON));
 
     VkAuthScene vkAuthScene = new VkAuthScene();
     vkAuthScene.addLoginListener(event -> {
